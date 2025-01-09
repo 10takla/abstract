5faa3a6b9941ac74693b5dc54574c45d7ce96f78
Abakar <letifovabakar50@gmail.com>
2025-01-09 06:25:42 +0300

    add distruct

--- lsp-server/src/distruct.rs
+++ lsp-server/src/distruct.rs
@@ -3,45 +3,52 @@ use lexer::lexer2::{
     AnyBlock, Args, AssignExpr, Block, FnC, Ident, Idents, Ignore, ImplC, Item, Items, Keyword,
     Literal, NamedBlock, Slicable, StructC, TraitC,
 };
-use std::iter::{empty, once};
 use tower_lsp::lsp_types::SemanticTokenType;
 
-// итератор для ленивого на всех вложенных уровнях
-type DistrIter = Box<dyn Iterator<Item = DistrItem>>;
+type DistrIter = Vec<DistrItem>;
 type DistrItem = ([usize; 2], SemanticTokenType);
 
-// итератор для ленивого прохода
-pub fn distruct_items<'a>(items: &'a Items) -> impl Iterator<Item = DistrItem> + 'a {
-    items.iter().flat_map(Distruct::distruct)
+// итератор для ленивого на всех вложенных уровнях
+pub fn distruct_items(items: &Items) -> impl Iterator<Item = DistrItem> {
+    let mut vec = vec![];
+    items.distruct(&mut vec);
+    vec.into_iter()
 }
 
-fn fast_once<'a>(v: &impl StartEnd, t: SemanticTokenType) -> impl Iterator<Item = DistrItem> + 'a {
-    once((v.start_end(), t))
+trait DI {
+    fn push_t(&mut self, value: &impl StartEnd, t: SemanticTokenType);
 }
-
-fn fast_box<'a>(v: &impl StartEnd, t: SemanticTokenType) -> DistrIter {
-    Box::new(fast_once(v, t))
-}
-
-pub trait Distruct {
-    fn distruct(&self) -> DistrIter {
-        Box::new(empty())
+impl DI for DistrIter {
+    fn push_t(&mut self, value: &impl StartEnd, t: SemanticTokenType) {
+        Vec::push(self, (value.start_end(), t));
     }
 }
 
+impl Distruct for Items {
+    fn distruct(&self, vec: &mut DistrIter) {
+        self.iter().for_each(|v| {
+            v.distruct(vec);
+        });
+    }
+}
+
+pub trait Distruct {
+    fn distruct(&self, vec: &mut DistrIter);
+}
+
 impl Distruct for Item {
-    fn distruct(&self) -> DistrIter {
+    fn distruct(&self, vec: &mut DistrIter) {
         use Item::*;
         match self {
-            FnC(v) => v.distruct(),
-            StructC(v) => v.distruct(),
-            TraitC(v) => v.distruct(),
-            ImplC(v) => v.distruct(),
-            AnyBlock(v) => v.distruct(),
-            AssignExpr(v) => v.distruct(),
-            Literal(v) => v.distruct(),
-            Idents(v) => v.distruct(),
-            Ignore(v) => v.distruct(),
+            FnC(v) => v.distruct(vec),
+            StructC(v) => v.distruct(vec),
+            TraitC(v) => v.distruct(vec),
+            ImplC(v) => v.distruct(vec),
+            AnyBlock(v) => v.distruct(vec),
+            AssignExpr(v) => v.distruct(vec),
+            Literal(v) => v.distruct(vec),
+            Idents(v) => v.distruct(vec),
+            Ignore(v) => v.distruct(vec),
         }
     }
 }
@@ -51,122 +58,129 @@ mod fn_c {
     use lexer::lexer2::FnHead;
 
     impl Distruct for FnC {
-        fn distruct(&self) -> DistrIter {
-            Box::new(self.0.distruct().chain(self.1.distruct()))
+        fn distruct(&self, vec: &mut DistrIter) {
+            self.0.distruct(vec);
+            self.1.distruct(vec);
         }
     }
 
     impl Distruct for FnHead {
-        // fn distruct(&self) -> DistrIter {
-        //     Box::new(
-        //         fast_box(&self.0, SemanticTokenType::KEYWORD)
-        //             .chain(fast_box(&self.1, SemanticTokenType::STRUCT))
-        //             .chain(self.2.color()),
-        //     )
-        // }
+        fn distruct(&self, vec: &mut DistrIter) {
+            vec.push_t(&self.0, SemanticTokenType::KEYWORD);
+            vec.push_t(&self.1, SemanticTokenType::STRUCT);
+            self.2.distruct(vec);
+        }
     }
 }
 
 impl Distruct for StructC {
-    fn distruct(&self) -> DistrIter {
-        Box::new(
-            fast_box(&self.0, SemanticTokenType::KEYWORD)
-                .chain(fast_box(&self.1, SemanticTokenType::FUNCTION))
-                // .chain(Box::new(self.2.color())),
-        )
+    fn distruct(&self, vec: &mut DistrIter) {
+        vec.push_t(&self.0, SemanticTokenType::KEYWORD);
+        vec.push_t(&self.1, SemanticTokenType::FUNCTION);
     }
 }
-impl Distruct for TraitC {}
-impl Distruct for ImplC {}
+impl Distruct for TraitC {
+    fn distruct(&self, vec: &mut DistrIter) {
+        vec.push_t(&self.0, SemanticTokenType::KEYWORD);
+        vec.push_t(&self.1, SemanticTokenType::VARIABLE);
+    }
+}
+impl Distruct for ImplC {
+    fn distruct(&self, vec: &mut DistrIter) {
+        vec.push_t(&self.0, SemanticTokenType::KEYWORD);
+        vec.push_t(&self.1, SemanticTokenType::FUNCTION);
+    }
+}
 impl Distruct for AnyBlock {
-    fn distruct(&self) -> DistrIter {
+    fn distruct(&self, vec: &mut DistrIter) {
         use self::AnyBlock::*;
         match self {
-            Block(v) => v.distruct(),
-            NamedBlock(v) => v.distruct(),
-            NamedDistrBlock(v) => Box::new(v.0.distruct().chain(fast_once(&v.1, BLOCK))),
-            DistrBlock(v) => fast_box(v, BLOCK),
+            Block(v) => v.distruct(vec),
+            NamedBlock(v) => v.distruct(vec),
+            NamedDistrBlock(v) => {
+                v.0.distruct(vec);
+                vec.push_t(&v.1, BLOCK);
+            }
+            DistrBlock(v) => {
+                vec.push_t(v, BLOCK);
+            }
         }
     }
 }
 impl Distruct for NamedBlock {
-    fn distruct(&self) -> DistrIter {
-        Box::new(fast_once(&self.0, BLOCK).chain(self.1.distruct()))
+    fn distruct(&self, vec: &mut DistrIter) {
+        vec.push_t(&self.0, BLOCK);
+        self.1.distruct(vec);
     }
 }
 
 impl Distruct for Block {
-    fn distruct(&self) -> DistrIter {
-        Box::new(
-            fast_once(&self.0, BLOCK)
-                // .chain(distruct_items(&self.1))
-                .chain(fast_once(&self.2, BLOCK)),
-        )
+    fn distruct(&self, vec: &mut DistrIter) {
+        vec.push_t(&self.0, BLOCK);
+        self.1.distruct(vec);
+        vec.push_t(&self.2, BLOCK);
     }
 }
 impl Distruct for AssignExpr {
-    fn distruct(&self) -> DistrIter {
+    fn distruct(&self, vec: &mut DistrIter) {
         use self::AssignExpr::*;
         match self {
-            Assign(v) => Box::new(v.0.distruct().chain(v.2.distruct())),
-            AssignAnd(v) => Box::new(v.0.distruct().chain(v.2.distruct())),
+            Assign(v) => {
+                v.0.distruct(vec);
+                v.2.distruct(vec);
+            }
+            AssignAnd(v) => {
+                v.0.distruct(vec);
+                v.2.distruct(vec);
+            }
         }
     }
 }
 impl Distruct for Literal {
-    fn distruct(&self) -> DistrIter {
+    fn distruct(&self, vec: &mut DistrIter) {
         use self::Literal::*;
         match self {
-            String(..) => fast_box(self, SemanticTokenType::STRING),
-            Number(..) => fast_box(self, SemanticTokenType::NUMBER),
+            String(..) => vec.push_t(self, SemanticTokenType::STRING),
+            Number(..) => vec.push_t(self, SemanticTokenType::NUMBER),
         }
     }
 }
 impl Distruct for Idents {
-    fn distruct(&self) -> DistrIter {
+    fn distruct(&self, vec: &mut DistrIter) {
         use self::Idents::*;
         match self {
-            Ident(v) => v.distruct(),
-            Keyword(v) => fast_box(v, SemanticTokenType::KEYWORD),
+            Ident(v) => v.distruct(vec),
+            Keyword(v) => vec.push_t(v, SemanticTokenType::KEYWORD),
         }
     }
 }
 impl Distruct for Ident {
-    fn distruct(&self) -> DistrIter {
-        fast_box(self, SemanticTokenType::VARIABLE)
+    fn distruct(&self, vec: &mut DistrIter) {
+        vec.push_t(self, SemanticTokenType::VARIABLE);
     }
 }
 
 impl Distruct for Keyword {
-    fn distruct(&self) -> DistrIter {
-        fast_box(self, SemanticTokenType::KEYWORD)
+    fn distruct(&self, vec: &mut DistrIter) {
+        vec.push_t(self, SemanticTokenType::KEYWORD);
     }
 }
 
-impl Distruct for Ignore {}
-
-trait Colorize {
-    fn color(&self) -> impl Iterator<Item = DistrItem> {
-        self.cl().into_iter().map(|(a, b)| (a.start_end(), b))
-    }
-    fn cl(&self) -> Vec<(&dyn StartEnd, SemanticTokenType)>;
+impl Distruct for Ignore {
+    fn distruct(&self, _: &mut DistrIter) {}
 }
 
-impl Colorize for Args {
-    fn cl(&self) -> Vec<(&dyn StartEnd, SemanticTokenType)> {
+impl Distruct for Args {
+    fn distruct(&self, vec: &mut DistrIter) {
         use Args::*;
         match self {
             StructArgsC(v) => {
-                vec![
-                    (&v.0, SemanticTokenType::FUNCTION),
-                    (&v.2, SemanticTokenType::FUNCTION),
-                ]
+                vec.push_t(&v.0, SemanticTokenType::FUNCTION);
+                vec.push_t(&v.2, SemanticTokenType::FUNCTION);
             }
             TupleArgsC(v) => {
-                vec![
-                    (&v.0, SemanticTokenType::FUNCTION),
-                    (&v.2, SemanticTokenType::FUNCTION),
-                ]
+                vec.push_t(&v.0, SemanticTokenType::FUNCTION);
+                vec.push_t(&v.2, SemanticTokenType::FUNCTION);
             }
         }
     }
